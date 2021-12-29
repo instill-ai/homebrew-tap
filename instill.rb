584956e48545b12f5ebf0aa5b198e008c2c1e6b4
Ping-Lin Chang <ping-lin.chang@instill.tech>
2021-12-29 02:09:39 +0000

    fix: fix instill cli formula

--- instill.rb
+++ instill.rb
@@ -3,11 +3,11 @@ class Instill < Formula
     homepage "https://github.com/instill-ai/cli"
     license "Apache-2.0"
     version "v0.1.0-alpha"
-    
-    if os.Mac?
+
+    if OS.mac?
         url "https://github.com/instill-ai/cli/releases/download/v0.1.0-alpha/instill_0.1.0-alpha_macOS_amd64.tar.gz"
         sha256 "2af62459dc74b53dd656ecd88cbe27b242a95c214f0028e63ecc365a6b3a4577"
-    end    
+    end
 
     if OS.linux? && Hardware::CPU.amd_family? && Hardware::CPU.is_64_bit?
         url "https://github.com/instill-ai/cli/releases/download/v0.1.0-alpha/instill_0.1.0-alpha_linux_amd64.tar.gz"
@@ -22,15 +22,15 @@ class Instill < Formula
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/instill-ai/cli/releases/download/v0.1.0-alpha/instill_0.1.0-alpha_linux_arm64.tar.gz"
         sha256 "a2dad62afecd853d14095ce8073c59df0b5202bf8d53e223a077b345aebd89bf"
-    end    
+    end
 
-    def install      
+    def install
       bin.install "bin/instill"
       (bash_completion/"instill").write `#{bin}/instill completion -s bash`
       (fish_completion/"instill.fish").write `#{bin}/instill completion -s fish`
       (zsh_completion/"_instill").write `#{bin}/instill completion -s zsh`
     end
-  
+
     test do
       assert_match "instill version #{version}", shell_output("#{bin}/instill --version")
     end
